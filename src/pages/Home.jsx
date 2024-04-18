7475a4fdba606b60de6cb358c375c8f192531abe
David <davidep@alumnos.iesgalileo.es>
2024-04-18 13:03:46 +0200

    test auth0

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,17 +3,18 @@ import {useAuth0} from '@auth0/auth0-react';
 import Navigation from "../components/Navigation.jsx";
 import {Divider} from "@nextui-org/react";
 import Footer from "../components/Footer.jsx";
-import {Redirect} from "react-router-dom";
+import {Navigate} from "react-router-dom";
 
 export default function Home() {
-    const {user, isAuthenticated, loginWithRedirect} = useAuth0();
+    const {user, isAuthenticated} = useAuth0();
 
     useEffect(() => {
         console.log(`Authentication status: ${isAuthenticated ? 'Authenticated' : 'Not authenticated'}`);
-    }, [isAuthenticated, loginWithRedirect]);
+        console.log(user);
+    }, []);
 
     if (!isAuthenticated) {
-        return <Redirect to="/"/>;
+        return <Navigate to="/"/>;
     }
 
     return (
